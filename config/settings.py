700d103086a70426e0d182c621df0988d0e954b2
lenis <fardintorkamand83@gmail.com>
2023-11-04 12:45:54 +0330

    Install django rest framework

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third Party Apps
+    'rest_framework',
     'debug_toolbar',
-    'store',
+    # Local Apps
+    'store.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
