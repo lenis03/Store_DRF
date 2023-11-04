05ef9231263dd5faebd552b305925e7b3bedf3f9
lenis <fardintorkamand83@gmail.com>
2023-11-04 16:56:42 +0330

    Clear project and some changes

--- store/models.py
+++ store/models.py
@@ -1,15 +1,26 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=500, blank=True)
-    top_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+    top_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, blank=True,related_name='+')
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categorise'
+        
+    def __str__(self):
+        return self.title
 
 
 class Discount(models.Model):
     discount = models.FloatField()
     description = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f'{self.discount} | {self.description}'
 
 
 class Product(models.Model):
@@ -18,18 +29,25 @@ class Product(models.Model):
     slug = models.SlugField()
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventory = models.IntegerField()
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
+    discounts = models.ManyToManyField(Discount, blank=True, related_name='products')
+
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
-    discounts = models.ManyToManyField(Discount, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     email = models.EmailField()
     phone_number = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 
 class Address(models.Model):
@@ -39,29 +57,52 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
 
 
+class UnpaidOrderManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Order.ORDER_STATUS_UNPAID)
+
+
 class Order(models.Model):
     ORDER_STATUS_PAID = 'p'
     ORDER_STATUS_UNPAID = 'u'
     ORDER_STATUS_CANCELED = 'c'
+
     ORDER_STATUS = [
-        (ORDER_STATUS_PAID,'Paid'),
-        (ORDER_STATUS_UNPAID,'Unpaid'),
-        (ORDER_STATUS_CANCELED,'Canceled'),
+        (ORDER_STATUS_PAID, 'Paid'),
+        (ORDER_STATUS_UNPAID, 'Unpaid'),
+        (ORDER_STATUS_CANCELED, 'Canceled'),
+
     ]
-    
+
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT, related_name='orders')
     datetime_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=1, choices=ORDER_STATUS, default=ORDER_STATUS_UNPAID)
+
+    objects = models.Manager()
+    unpaid_orders = UnpaidOrderManager()
+
+    def __str__(self):
+        return f'order id: {self.id}'
 
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='order_items')
-    quantity = models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField(default=1)
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
         unique_together = [['order', 'product']]
+
+
+class CommentManager(models.Manager):
+    def get_approved(self):
+        return self.get_queryset().filter(status=Comment.COMMENT_STATUS_APPROVED)
+
+
+class ApprovedCommentManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Comment.COMMENT_STATUS_APPROVED)
 
 
 class Comment(models.Model):
@@ -75,10 +116,13 @@ class Comment(models.Model):
     ]
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_length=255)
-    body = models.TextField()
+    name = models.CharField(max_length=100)
+    body = models.TextField(max_length=500)
     datetime_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=2, choices=COMMENT_STATUS, default=COMMENT_STATUS_WAITING)
+
+    objects = CommentManager()
+    approved = ApprovedCommentManager()
 
 
 class Cart(models.Model):
@@ -86,9 +130,9 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
-    quantity = models.PositiveSmallIntegerField()
+    cart = models.ForeignKey(Cart, on_delete=models.PROTECT, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='cart_items')
+    quantity = models.PositiveSmallIntegerField(default=1)
 
     class Meta:
         unique_together = [['cart', 'product']]
