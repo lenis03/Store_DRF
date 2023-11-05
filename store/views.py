2f4144213c14dd46a3580c51b509fee17f9379bc
lenis <fardintorkamand83@gmail.com>
2023-11-05 18:08:25 +0330

    Create api view product_list & product_detail

--- store/views.py
+++ store/views.py
@@ -1,1 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from . serializer import ProductSerializer
+from .models import Product
+
+
+@api_view()
+def product_list(request):
+    products_queryset = Product.objects.all()
+    serializer = ProductSerializer(products_queryset, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
