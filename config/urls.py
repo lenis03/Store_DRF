bb0938f5fd7b55439324992434fdb8e1a63e3db0
lenis <fardintorkamand83@gmail.com>
2023-11-05 17:58:12 +0330

    Create product list& product detail url

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('store/', include('store.urls')),
+    path('store/', include('store.urls', namespace='store')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from store import views
+
+app_name = 'store'
 
 urlpatterns = [
-
+    path('products/', views.product_list),
+    path('product/<int:id>/', views.product_detail),
 ]
