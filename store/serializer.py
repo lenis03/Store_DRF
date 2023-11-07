e0817ac3aa2cecf7c24e21e05e18a8d36c8ad369
lenis <fardintorkamand83@gmail.com>
2023-11-07 15:08:56 +0330

    Delete calculate dollors to rials

--- store/serializer.py
+++ store/serializer.py
@@ -3,8 +3,6 @@ from rest_framework import serializers
 
 from store.models import Product
 
-DOLLORS_TO_RIALS = 500000
-
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -12,10 +10,6 @@ class ProductSerializer(serializers.Serializer):
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
     unit_price_after_tax = serializers.SerializerMethodField()
     inventory = serializers.IntegerField()
-    price_rials = serializers.SerializerMethodField()
-
-    def get_price_rials(self, product: Product):
-        return int(product.unit_price * DOLLORS_TO_RIALS)
 
     def get_unit_price_after_tax(self, product: Product):
         return round(product.unit_price * Decimal(1.09), 2)
