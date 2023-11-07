464b1a91d9fe34e2ae14292a4a7506ec936f77d1
lenis <fardintorkamand83@gmail.com>
2023-11-07 14:24:18 +0330

    Creaate field price_rials for change $ to rials

--- store/serializer.py
+++ store/serializer.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 
+from store.models import Product
+
+DOLLORS_TO_RIALS = 500000
+
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=255)
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
     inventory = serializers.IntegerField()
+    price_rials = serializers.SerializerMethodField()
+
+    def get_price_rials(self, product: Product):
+        return int(product.unit_price * DOLLORS_TO_RIALS)
