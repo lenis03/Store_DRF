9202c98d60199a3771c416cc1de27acad9be5f93
lenis <fardintorkamand83@gmail.com>
2023-11-08 12:05:59 +0330

    Add category field&select related for optimizing Q

--- store/serializer.py
+++ store/serializer.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from rest_framework import serializers
 
-from store.models import Product
+from .models import Category, Product
 
 
 class ProductSerializer(serializers.Serializer):
@@ -10,6 +10,7 @@ class ProductSerializer(serializers.Serializer):
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
     unit_price_after_tax = serializers.SerializerMethodField()
     inventory = serializers.IntegerField()
+    category = serializers.StringRelatedField()
 
     def get_unit_price_after_tax(self, product: Product):
         return round(product.unit_price * Decimal(1.09), 2)
