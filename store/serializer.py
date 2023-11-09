a04a8dcd6a2ac256253f96413c6f25f4fe3ef4bc
lenis <fardintorkamand83@gmail.com>
2023-11-09 13:08:21 +0330

    Use model serialier for product

--- store/serializer.py
+++ store/serializer.py
@@ -9,16 +9,29 @@ class CategorySerializer(serializers.Serializer):
     description = serializers.CharField(max_length=255)
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+class ProductSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=255, source='name')
+    price = serializers.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        source='unit_price'
+        )
     unit_price_after_tax = serializers.SerializerMethodField()
-    inventory = serializers.IntegerField()
     category = serializers.HyperlinkedRelatedField(
         queryset=Category.objects.all(),
         view_name='store:category_detail',
     )
+
+    class Meta:
+        model = Product
+        fields = [
+            'id',
+            'title',
+            'price',
+            'unit_price_after_tax',
+            'inventory',
+            'category'
+            ]
 
     def get_unit_price_after_tax(self, product: Product):
         return round(product.unit_price * Decimal(1.09), 2)
