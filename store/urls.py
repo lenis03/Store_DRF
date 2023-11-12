831ed121e073dedf1c3ecb2cbc31d496ae315008
lenis <fardintorkamand83@gmail.com>
2023-11-12 15:41:16 +0330

    Add path categories

--- store/urls.py
+++ store/urls.py
@@ -7,5 +7,6 @@ app_name = 'store'
 urlpatterns = [
     path('products/', views.product_list, name='product_list'),
     path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path('categories/', views.category_list, name='category_list'),
     path('categories/<int:pk>/', views.category_detail, name='category_detail'),
 ]
