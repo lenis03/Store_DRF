840053c5de821dccd1b86ca5c68bdc830ec9b074
lenis <fardintorkamand83@gmail.com>
2023-11-15 17:17:45 +0330

    Inheritance from RetrieveUpdateDestroy for ProductDetail

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from . serializer import CategorySerializer, ProductSerializer
 from .models import Category, Product
@@ -16,33 +16,19 @@ class ProductList(ListCreateAPIView):
         return {'request': self.request}
 
 
-class ProductDetail(APIView):
-    def setup(self, request, *args, **kwargs):
-        self.product = get_object_or_404(
-            Product
-            .objects
-            .select_related('category'),
-            pk=kwargs['pk'])
-        return super().setup(request, *args, **kwargs)
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.select_related('category').all()
 
-    def get(self, request, *args, **kwargs):
-        serializer = ProductSerializer(self.product, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, *args, **kwargs):
-        serializer = ProductSerializer(self.product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, *args, **kwargs):
-        if self.product.order_items.count() > 0:
+    def delete(self, request, pk):
+        product = get_object_or_404(Product.objects.select_related('category'),pk=pk)
+        if product.order_items.count() > 0:
             return Response({
                 'error':
                     'There is some order items including this product.'
                     'Please remove them first'},
                     status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        self.product.delete()
+        product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
