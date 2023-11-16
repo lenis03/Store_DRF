614d277d39264cb9b110c67ced1fb501d60540a5
lenis <fardintorkamand83@gmail.com>
2023-11-16 13:23:46 +0330

    Create CommentViewSet

--- store/views.py
+++ store/views.py
@@ -4,8 +4,8 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from . serializer import CategorySerializer, ProductSerializer
-from .models import Category, Product
+from . serializer import CategorySerializer, ProductSerializer, CommentSerializer
+from .models import Category, Comment, Product
 
 
 class ProductViewSet(ModelViewSet):
@@ -47,3 +47,11 @@ class CategoryViewSet(ModelViewSet):
                 }, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         category.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        product_pk = self.kwargs['product_pk']
+        return Comment.objects.filter(product_id=product_pk).all()
