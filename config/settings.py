25579e73edec65354b50f59ef277b41b3ce64e43
lenis <fardintorkamand83@gmail.com>
2023-11-17 16:09:49 +0330

    Install django_filters

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third Party Apps
+    'django_filters'
     'rest_framework',
     'debug_toolbar',
     # Local Apps
