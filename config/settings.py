60bc2ff69543c13028e011ec345ffdcd4a9a63b3
lenis <fardintorkamand83@gmail.com>
2023-11-29 13:07:13 +0330

    Change refresh token lifetime

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,8 @@ REST_FRAMEWORK = {
 # djangorestframework_simplejwt config
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT', ),
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
 # Djoser config
