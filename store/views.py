3a722eed227c029d38ae5626f312d402e08e2275
lenis <fardintorkamand83@gmail.com>
2023-12-01 00:44:26 +0330

    Impelemt put method for action method me CustomerViewSet

--- store/views.py
+++ store/views.py
@@ -107,9 +107,15 @@ class CustomerViewSet(ModelViewSet):
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
 
-    @action(detail=False)
+    @action(detail=False, methods=['GET', 'PUT'])
     def me(self, request):
         user_id = request.user.id
         customer = Customer.objects.get(user_id=user_id)
-        serializer = CustomerSerializer(customer)
-        return Response(serializer.data)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(instance=customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
