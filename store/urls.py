600f97e5e90e8dbca652bae9eb7bc908adaf39ae
Lenis <fardintorkamand83@gmail.com>
2023-12-09 08:20:26 -0500

    Add orders to router url

--- store/urls.py
+++ store/urls.py
@@ -11,6 +11,7 @@ router.register('products', views.ProductViewSet, basename='product')
 router.register('categories', views.CategoryViewSet, basename='category')
 router.register('carts', views.CartViewSet, basename='cart')
 router.register('customers', views.CustomerViewSet, basename='customer')
+router.register('orders', views.OrderViewSet, basename='order')
 
 products_router = routers.NestedDefaultRouter(
     router,
